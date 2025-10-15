0f9148fec092d8eabe04a84a8ada2c15b5c1b27e
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-10-15 14:30:40 +0300

    Implementing a simple file-based router

--- includes/router.php
+++ includes/router.php
@@ -1,11 +1,45 @@
 <?php
 
 declare(strict_types=1);
+const ALLOWRD_METHODS = ['GET', 'POST'];
+const INDEX_URI = '';
+const INDEX_ROUTE = 'index';
+
+function normailzeUri(string $uri): string {
+  $uri = strtolower(trim($uri, '/'));
+  return $uri === INDEX_URI ? INDEX_ROUTE : $uri;
+}
+
+function getFilePath(string $uri, string $method): string {
+  return ROUTES_DIR . '/' . normailzeUri($uri) . '_' . strtolower( $method) . '.php';
+}
+
+function notFound(): void {
+  http_response_code(404);
+  echo "404 Not Found";
+  exit;
+}
 
 function dispatch(string $uri, string $method): void {
   // 1- normalize the URI: GET /geustbook -> routes/guestbook_get.php
+  $uri = normailzeUri($uri);
+  $method = strtoupper($method);
+  // var_dump($uri);die;
+
   // 2- GET|POST - reuturn 404
+  if(!in_array($method, ALLOWRD_METHODS)) {
+    // notFound 404
+    notFound();
+  }
+
   // 3- file path - PHP file path
+  $filePath = getFilePath($uri, $method);
+  if(file_exists($filePath)) {
+    include($filePath);
+    return;
+  }
+
+  notFound();
   // 4- if this file exists, if not 404
   // 5- Handle the route by including the php file
 }
--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 declare(strict_types=1);
 
 const INCLUDES_DIR = __DIR__ . '/../includes';
+const ROUTES_DIR = __DIR__ . '/../routes';
 
 session_start();
 require_once INCLUDES_DIR . '/router.php';
 
 // handle requests
+dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
